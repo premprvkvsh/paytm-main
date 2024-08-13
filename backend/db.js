f8b817ee57338ab22b3790b8236ef3ab65f25314
Prem <pprakash312003@gmail.com>
2024-08-14 01:44:17 +0530

    eligate solution

--- backend/db.js
+++ backend/db.js
@@ -4,11 +4,34 @@ mongoose.connect("mongodb://localhost:27017/paytm");
 
 
 const userSchema = mongoose.Schema({
-    username: string,
-    password: string,
-    firstName: string,
-    lastName: string
-})
+    username: {
+        type: string,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLenght: 30
+    },
+    password: {
+        type: string,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: string,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: string,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+
+});
 
 const User = mongoose.model("User", userSchema);
 
