4b2ab8e263251c845706e9313b72f55a810268d0
Prem <pprakash312003@gmail.com>
2024-08-14 16:17:59 +0530

    middleware.js added and index.js updated

---
+++ backend/middleware.js
@@ -0,0 +1,34 @@
+const {
+    JWT_SECRET
+} = require("./routes/config")
+
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+    const auth = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startWith('Bearer')) {
+        return res.status(403).json({});
+
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if (decoded.userId) {
+            req.userId = decoded.userId;
+            next();
+        } else {
+            return res.status(403).json({});
+        }
+    } catch (err) {
+        return res.status(403).json({});
+    }
+
+};
+
+module.exports = {
+    authMiddleware
+}
