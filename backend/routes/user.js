09e2065ef0da319228243d308ae045e20d905463
Prem <pprakash312003@gmail.com>
2024-08-14 16:04:10 +0530

    added signin route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Zod = require("zod");
+const zod = require("zod");
 const {
     JWT_SECRET
 } = require("./config");
@@ -30,7 +30,7 @@ router.post("/Signup", async function (req, res) {
         })
     }
 
-    const user = User.findOne({
+    const user = await User.findOne({
         username: body.username
     })
 
@@ -50,7 +50,53 @@ router.post("/Signup", async function (req, res) {
 
     res.json({
         message: "User created successfully",
-        token: "token"
+        token: token
     })
 
 })
+
+router.post("/Signin", async function (req, res) {
+    const body = req.body;
+    const {
+        success
+    } = signinSchema.safeParse(req.body);
+    if (!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: body.username,
+        password: body.password
+    })
+
+
+    if (user._id) {
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const dbUser = await User.create(body);
+
+    if (user) {
+        const token = jwt.sign({
+            userId: dbUser._id
+
+        }, JWT_SECRET);
+
+        res.json({
+
+            token: token
+        })
+        return;
+
+    }
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
+
+module.export = router;
